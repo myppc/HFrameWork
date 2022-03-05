691e0811a506ebee9b3601f3d7aa28f26da6c133
myppc <965167409@qq.com>
2022-03-05 22:56:55 +0800

    计时器

---
+++ HFrameWork/Assets/HFrameWork/Script/Tick/Ticker.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Ticker 
+{
+    private TickerParam _param;
+    private int _callCount = 0;
+    private bool _isDestroy;
+    public Ticker(TickerParam param)
+    {
+        _param = param;
+    }
+
+    public int GetTickerUid()
+    {
+        return _param.uid;
+    }
+
+    public object GetTickerTag()
+    {
+        return _param.Tag;
+    }
+
+    public void OnFrameTick(int curFrame)
+    {
+        var dis = curFrame - _param.StartFrame;
+        if(dis >0 && dis% _param.TickDelay == 0)
+        {
+            _callCount += 1;
+            _param.OnTickCallBack(curFrame);
+            if(_param.LoopCount != 0 && _callCount >= _param.LoopCount)
+            {
+                _isDestroy = true;
+            }
+        }
+    }
+
+    public bool IsDestroy()
+    {
+        return _isDestroy;
+    }
+
+    public void Destroy()
+    {
+        _param.OnCancelCallBack?.Invoke();
+    }
+}
